ebd18f91a85f1c56997f9d3f8bea7e50901f58e3
pixbrand-ui <74093225+pixbrand-ui@users.noreply.github.com>
2025-09-08 11:21:26 +0530

    Add /submittodoitem backend route with MongoDB support

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# MongoDB Connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client.todoDB
+collection = db.items
+
 @app.route('/')
-def hello_world():
-    return "Welcome to the Home Page!"
+def home():
+    return render_template('todo.html')
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    item_name = request.form.get('itemName')
+    item_desc = request.form.get('itemDescription')
+
+    if item_name and item_desc:
+        collection.insert_one({
+            'name': item_name,
+            'description': item_desc
+        })
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
